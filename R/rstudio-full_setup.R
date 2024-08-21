e743b5b449fbf262d7501e2c26ad3803a695a2e4
Daniel Lill <daniel.lill@intiquan.com>
2024-08-21 18:25:29 +0200

    set of defensive addins

--- R/rstudio-full_setup.R
+++ R/rstudio-full_setup.R
@@ -4,7 +4,7 @@
 #' @export
 #'
 #' @examples
-printRSShortcuts <- function() {
+printRSShortcuts <- function(FLAGdefensive = TRUE) {
   fl <- system.file("setup_rstudio/keybindings/addins.json", package = "RSAddins")
   text <- readLines(fl)
   text <- text[-c(1, length(text))]
@@ -26,11 +26,14 @@ printRSShortcuts <- function() {
 #'
 #' @export
 #' @importFrom stats setNames
-installRSkeybindings <- function(FLAGoverwrite = FALSE){
+installRSkeybindings <- function(FLAGoverwrite = FALSE, FLAGdefensive = TRUE){
   keybindings_paths <- c("~/.R/rstudio/keybindings", "~/.config/rstudio/keybindings")
   for (keybindings_path in keybindings_paths) {
     if (!dir.exists(keybindings_path)) dir.create(keybindings_path, FALSE, TRUE)
-    keybindings_files <- list.files(system.file("setup_rstudio/keybindings", package = "RSAddins"), "json$", F, T)
+    path <- ifelse(FLAGdefensive, 
+                   system.file("setup_rstudio/keybindings_defensive", package = "RSAddins")
+                   system.file("setup_rstudio/keybindings", package = "RSAddins"))
+    keybindings_files <- list.files(path, "json$", F, T)
     wup <- vapply(stats::setNames(nm = keybindings_files), file.copy, to = keybindings_path, overwrite = FLAGoverwrite, FUN.VALUE = TRUE)
   }
   if (!any(wup)) cat("No keybindings were installed. Do you need to set FLAGoverwrite to TRUE?")
