92e5c2f7de172c049c9f6b4c90fc3da515bfdf3f
Daniel Lill <daniel.lill@intiquan.com>
2024-08-21 18:50:51 +0200

    defensive also for printing

--- R/rstudio-full_setup.R
+++ R/rstudio-full_setup.R
@@ -5,7 +5,10 @@
 #'
 #' @examples
 printRSShortcuts <- function(FLAGdefensive = TRUE) {
-  fl <- system.file("setup_rstudio/keybindings/addins.json", package = "RSAddins")
+  fl <- ifelse(FLAGdefensive, 
+                 system.file("setup_rstudio/keybindings_defensive/addins.json", package = "RSAddins"),
+                 system.file("setup_rstudio/keybindings/addins.json", package = "RSAddins"))
+  
   text <- readLines(fl)
   text <- text[-c(1, length(text))]
   text <- gsub('"',"", text)
@@ -31,7 +34,7 @@ installRSkeybindings <- function(FLAGoverwrite = FALSE, FLAGdefensive = TRUE){
   for (keybindings_path in keybindings_paths) {
     if (!dir.exists(keybindings_path)) dir.create(keybindings_path, FALSE, TRUE)
     path <- ifelse(FLAGdefensive, 
-                   system.file("setup_rstudio/keybindings_defensive", package = "RSAddins")
+                   system.file("setup_rstudio/keybindings_defensive", package = "RSAddins"),
                    system.file("setup_rstudio/keybindings", package = "RSAddins"))
     keybindings_files <- list.files(path, "json$", F, T)
     wup <- vapply(stats::setNames(nm = keybindings_files), file.copy, to = keybindings_path, overwrite = FLAGoverwrite, FUN.VALUE = TRUE)
